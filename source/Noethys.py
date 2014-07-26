ffbc1ce1647e2998090f9b562b0e9d86c19b9170
cg90160 <cg90@free.fr>
2014-07-26 23:39:21 +0200

    Mise à jour auto Linux depuis github

--- source/Noethys.py
+++ source/Noethys.py
@@ -4022,7 +4022,7 @@ class MainFrame(wx.Frame):
         try :
             if "linux" in sys.platform :
                 # Version Debian
-                fichierVersions = urllib2.urlopen('http://www.noethys.com/fichiers/linux/Versions.txt', timeout=5)
+                fichierVersions = urllib2.urlopen('https://raw.githubusercontent.com/Noethys/Noethys/master/source/Versions.txt', timeout=5)
             else:
                 # Version Windows
                 fichierVersions = urllib2.urlopen('http://www.noethys.com/fichiers/windows/Versions.txt', timeout=5)
