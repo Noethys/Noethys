c137296548f886ec7165c1d3c94e0c671757998f
Noethys <info@noethys.com>
2016-09-09 21:50:40 +0200

    Fix données portail dans export activité

--- noethys/Ol/OL_Activites.py
+++ noethys/Ol/OL_Activites.py
@@ -78,6 +78,9 @@ class Exporter(UTILS_Export_tables.Exporter):
         self.ExporterTable("combi_tarifs_unites", self.FormateCondition("IDtarif", self.dictID["tarifs"]))
         self.ExporterTable("tarifs_lignes", "IDactivite=%d" % ID)
         self.ExporterTable("questionnaire_filtres", self.FormateCondition("IDtarif", self.dictID["tarifs"]))
+        # Portail
+        self.ExporterTable("portail_periodes", "IDactivite=%d" % ID)
+        self.ExporterTable("portail_unites", "IDactivite=%d" % ID)
 
         # Correspondances spéciales
         self.correspondances_speciales.append({"table" : "etiquettes", "champ" : "parent", "champ_reference" : "IDetiquette"})
