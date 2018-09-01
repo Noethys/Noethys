518b885284dc184a007f6071694a09ce579c2e40
Noethys <info@noethys.com>
2018-09-01 16:07:59 +0200

    Augmente taille champ email individu

--- noethys/Data/DATA_Tables.py
+++ noethys/Data/DATA_Tables.py
@@ -61,7 +61,7 @@ DB_DATA = {
                                     ("tel_domicile", "VARCHAR(50)", u"Tel domicile de la personne"),  
                                     ("tel_mobile", "VARCHAR(50)", u"Tel mobile perso de la personne"),  
                                     ("tel_fax", "VARCHAR(50)", u"Fax perso de la personne"),  
-                                    ("mail", "VARCHAR(50)", u"Email perso de la personne"),
+                                    ("mail", "VARCHAR(200)", u"Email perso de la personne"),
 
                                     ("travail_tel_sms", "INTEGER", u"SMS autorisé (0/1)"),
                                     ("tel_domicile_sms", "INTEGER", u"SMS autorisé (0/1)"),
