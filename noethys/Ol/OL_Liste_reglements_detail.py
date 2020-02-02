021e406b271d4d76f8a380689244da3f3bc9c703
Noethys <Noethys@users.noreply.github.com>
2020-02-02 22:33:14 +0100

    Désactive double-clic dans liste détaillée règlements

--- noethys/Ol/OL_Liste_reglements_detail.py
+++ noethys/Ol/OL_Liste_reglements_detail.py
@@ -127,12 +127,8 @@ class ListView(FastObjectListView):
         self.nom_fichier_liste = __file__
         FastObjectListView.__init__(self, *args, **kwds)
         # Binds perso
-        self.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.OnItemActivated)
         self.Bind(wx.EVT_CONTEXT_MENU, self.OnContextMenu)
 
-    def OnItemActivated(self,event):
-        self.Modifier(None)
-                
     def InitModel(self):
         self.donnees = self.GetTracks()
 
