b904978a40af291c8203baf64ba0cf46ff2093f1
Noethys <Noethys@users.noreply.github.com>
2020-10-28 18:41:25 +0100

    Ajout du Captcha dans Connecthys

--- noethys/Utils/UTILS_Portail_synchro.py
+++ noethys/Utils/UTILS_Portail_synchro.py
@@ -175,6 +175,7 @@ class Synchro():
         liste_lignes.append(Ecrit_ligne("PREFIXE_TABLES", self.dict_parametres["prefixe_tables"], type_valeur=str))
         liste_lignes.append(Ecrit_ligne("DEBUG", self.dict_parametres["mode_debug"], type_valeur=bool))
         liste_lignes.append(Ecrit_ligne("TALISMAN", self.dict_parametres["talisman"], type_valeur=bool))
+        liste_lignes.append(Ecrit_ligne("CAPTCHA", self.dict_parametres["captcha"], type_valeur=int))
 
         # Paramètres SMTP pour Flask-mail
         if self.dict_parametres["email_type_adresse"] > 0 :
